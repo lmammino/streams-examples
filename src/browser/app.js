0f9cd57002355576e68e21e6a6132b8f9e7d4a71
Luciano <lucianomammino@gmail.com>
2019-03-05 00:04:11 +0000

    cleanup

--- src/browser/app.js
+++ src/browser/app.js
@@ -1,6 +1,5 @@
 'use strict'
 
-// const { Stream } = require('readable-stream')
 const EmojiStream = require('../emoji-stream')
 const Uppercasify = require('../uppercasify')
 const DOMAppend = require('../dom-append')
@@ -11,7 +10,3 @@ const uppercasify = new Uppercasify()
 const append = new DOMAppend(list, 'li')
 
 emoji.pipe(uppercasify).pipe(append)
-
-// Stream.pipeline(emoji, uppercasify, append, err => {
-//   alert(err ? `Something went wrong: ${err}` : 'All done!')
-// })
